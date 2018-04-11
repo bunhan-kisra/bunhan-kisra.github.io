a63b79c953fb23dfee445600c0fb39fd233bd2ac
bunhan.kisra <bunhan.n@kisracorp.com>
2018-04-11 21:10:52 +0700

    use netpie v1

--- index.js
+++ index.js
@@ -12,60 +12,103 @@ var microgear = MicroGear.create({
 });
 
 microgear.connect(APPID);
+var myId = 'id'+ (Math.floor(Math.random() * 1000000) + 1)
 
 microgear.on('connected', function() {
     console.log('Connected...');
-    microgear.setAlias("mygear");
-    setInterval(function() {
-        microgear.chat('mygear', 'Hello world.'+ new Date());
-    },1000);
+    // var myId = 'id'+ (Math.floor(Math.random() * 1000000) + 1)
+    microgear.setAlias(myId);
+    console.log('my id %s', myId)
+    // setInterval(function() {
+    //     microgear.chat('mygear', 'Hello world.'+ new Date());
+    // },1000);
+
+    // document.querySelector('#btn1').addEventListener('click', function (ev) {
+    //     ev.preventDefault()
+    //     // p.signal(JSON.parse(document.querySelector('#incoming').value))
+    //     // console.log(document.querySelector('#myTextarea').value)
+    //     var input = document.querySelector('#myTextarea').value.toString()
+    //     console.log(input)
+    //     microgear.chat(input, 'Hello world.'+ new Date());
+    // })
+
 });
 
-microgear.on('message', function(topic,body) {
-    console.log('incoming : '+topic+' : '+body);
-});
+// microgear.on('message', function(topic,body) {
+//     console.log('incoming : '+topic+' : '+body);
+//     if (body.toString().indexOf('getSinnal') > -1) {
+//         var aname = body.toString().split(':')
+//         var senderName = aname[1]
+//         microgear.chat(senderName, sinnalData);
+//     }
+// });
 
 microgear.on('closed', function() {
     console.log('Closed...');
 });
 
+var sinnalData;
+var hostS;
 // get video/voice stream
 navigator.getUserMedia({ video: true, audio: true }, gotMedia, function () {})
 
 function gotMedia (stream) {
-var p = new Peer({ initiator: location.hash === '#1', trickle: false, stream: stream})
+    var p = new Peer({ initiator: location.hash === '#1', trickle: false, stream: stream})
 
-p.on('error', function (err) { console.log('error', err) })
+    p.on('error', function (err) { console.log('error', err) })
 
-p.on('signal', function (data) {
-  console.log('SIGNAL', JSON.stringify(data))
-  document.querySelector('#outgoing').textContent = JSON.stringify(data)
-})
+    p.on('signal', function (data) {
+        sinnalData = JSON.stringify(data)
+        console.log('SIGNAL', sinnalData)
+        document.querySelector('#outgoing').textContent = sinnalData
+        if (location.hash !== '#1') {
+            microgear.chat(hostS, sinnalData);
+        }
+    })
 
-document.querySelector('form').addEventListener('submit', function (ev) {
-  ev.preventDefault()
-  p.signal(JSON.parse(document.querySelector('#incoming').value))
-})
-document.querySelector('button').addEventListener('click', function (ev) {
-  ev.preventDefault()
-  // p.signal(JSON.parse(document.querySelector('#incoming').value))
-    console.log(document.querySelector('#myTextarea').value)
-})
+    document.querySelector('form').addEventListener('submit', function (ev) {
+        ev.preventDefault()
+        p.signal(JSON.parse(document.querySelector('#incoming').value))
+    })
 
-p.on('connect', function () {
-  console.log('CONNECT')
-  p.send('whatever' + Math.random())
-})
 
-p.on('data', function (data) {
-  console.log('data: ' + data)
-})
+    p.on('connect', function () {
+        console.log('CONNECT')
+        p.send('whatever' + Math.random())
+    })
 
-p.on('stream', function (stream) {
-    // got remote video stream, now let's show it in a video tag
-    var video = document.querySelector('video')
-    video.src = window.URL.createObjectURL(stream)
-    video.play()
-  })
+    p.on('data', function (data) {
+        console.log('data: ' + data)
+    })
+
+    p.on('stream', function (stream) {
+        // got remote video stream, now let's show it in a video tag
+        var video = document.querySelector('video')
+        video.src = window.URL.createObjectURL(stream)
+        video.play()
+    })
+
+    //
+    microgear.on('message', function(topic,body) {
+    console.log('incoming : '+topic+' : '+body);
+    if (body.toString().indexOf('getSinnal') > -1) {
+        var aname = body.toString().split(':')
+        var senderName = aname[1]
+        microgear.chat(senderName, sinnalData);
+    } else {
+        p.signal(JSON.parse(body.toString()))
+    }
+});
 
 }
+
+
+document.querySelector('#btn1').addEventListener('click', function (ev) {
+        ev.preventDefault()
+        // p.signal(JSON.parse(document.querySelector('#incoming').value))
+        // console.log(document.querySelector('#myTextarea').value)
+        var input = document.querySelector('#myTextarea').value.toString()
+        console.log(input)
+        hostS = input
+        microgear.chat(input, 'getSinnal:'+ myId);
+    })
